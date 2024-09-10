37a0f23b5b65e6b35b574f2791bd49f1bba46a4c
Caroline Capuano <carolinecapuano@MacBook-Air-2.local>
2024-09-10 12:19:59 -0400

    cow_simulator.py

---
+++ cow_simulator.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def compute_forces():
+    gravity = 9.8 #m/s^2
+    mass = 1000 #kg
+    force_gravity = np.array([0,mass*gravity])
+    position = 
+    velocity =
+    force_wind_resistance = 
+
+def updated_position_velocity():
+
+
+def energy():
+
+
+
+def main():
+
+
+
+def plot():
