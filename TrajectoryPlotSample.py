b8e0a97e3f0acac72f3a51a6da266570e53f65c7
Jasmine Watt <jasminewatt8@gmail.com>
2024-09-11 19:14:58 -0400

    TrajectoryPlotSample.py

--- TrajectoryPlotSample.py
+++ TrajectoryPlotSample.py
@@ -3,15 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 #%% Cell 2
 mo = 1500
-q = 2.5
 u = 6000
-vo = 0
+vo = 200 #m/s
 g = 9.8
 y0 = 1000 #m
-t = np.arange(0,int(1500/2.5))
+t = np.arange(0,int(1000/10))
 
 def v(t):
-    return vo -g*t
+    return vo - g*t
 
 def y(t):
     return y0 + vo*t - 0.5*g*(t**2)
