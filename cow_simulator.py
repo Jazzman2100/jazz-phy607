8ffb926a29461e2f98a103e24f284543461835e9
Jasmine Watt <jasminewatt8@gmail.com>
2024-09-11 19:17:53 -0400

    cow_simulator.py

--- cow_simulator.py
+++ cow_simulator.py
@@ -94,3 +94,23 @@ def plot():
     velocity = np.array(velocity)
 
     plt.plot()
+
+# %% Cell 1
+import numpy as np
+import matplotlib.pyplot as plt
+#%% Cell 2
+mo = 1000 #kg
+vo = 200 #m/s
+g = 9.8 #m/s^2
+y0 = 1000 #m
+t = np.arange(0,int(1000/10))
+
+def v(t):
+    return vo - g*t
+
+def y(t):
+    return y0 + vo*t - 0.5*g*(t**2)
+
+plt.plot(t,y(t))
+plt.grid()
+plt.show()
