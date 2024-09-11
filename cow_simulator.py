d60852ae4f89c6fbdbb43cec2dc8352a50c5fa6c
Caroline Capuano <carolinecapuano@MacBook-Air-2.lan>
2024-09-11 18:19:46 -0400

    cow_simulator.py

--- cow_simulator.py
+++ cow_simulator.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# def compute_forces(position, velocity):
-gravity = 9.8 #m/s^2
-mass = 1000 #kg
-force_gravity = mass*gravity
-force_wind_resistance = velocity**2
-total_force = -force_wind_resistance + force_gravity
-# return total_force
+def compute_forces(position, velocity):
+    gravity = 9.8 #m/s^2
+    mass = 1000 #kg
+    force_gravity = mass*gravity
+    force_wind_resistance = velocity**2
+    total_force = -force_wind_resistance + force_gravity
+    return total_force
 
 # no wind resistance for now
 # prompt user to input the initial velocity of x
@@ -55,19 +55,25 @@ def main():
     position = np.array([0.0,1000.0])
     velocity = np.array([10.0,0.0])
     mass = 1000
+    #if we need this: wind_resistance_constant = 1
     time_step =0.1
     time = 0
     max_time = 1000
 
     #make lists for plotting
 
-    position = []
-    velocity = []
+    position_x = []
+    position_y =[]
+    velocity_x = []
+    velocity_y= []
     energy = []
 
     while position[1]>0 and time<max_time:
-        force = compute_forces(position, velocity, mass, force_wind_resistance)
-        position,velocity = updat
+        force_x = compute_forces_x(position_x, velocity_x, mass, force_wind_resistance)
+        force_y = compute_forces_y(position_y, velocity_y, mass, force_wind_resistance)
+        positions.append(position.copy())
+        velocity.append(velocity.copy())
+        energy.append((potential_energy, kinetic_energy, total_energy))
 
         time += time_step
 
